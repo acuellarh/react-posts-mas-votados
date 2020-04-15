713f3ffdb470cdf00f04f83f40a210510bc82ffd
Alejandro <acuellarh@gmail.com>
2020-04-15 17:50:27 -0500

    sort asc and desc the post methods and buttons without css styles

--- src/App.js
+++ src/App.js
@@ -7,10 +7,26 @@ import { FaAngleDown } from "react-icons/fa";
 // Modifica el componente App para implmentar la funcionalidad requerida
 
 class App extends Component {  
+
+  constructor(props){
+    super(props)
+    this.state = { posts }    
+  }
+
+  sortByVotesAsc(){
+    this.setState({
+      posts: posts.sort((a,b) => (a.votes - b.votes))
+    });
+  }
+
+  sortByVotesDesc(){
+    this.setState({   
+      posts: posts.sort((a,b) => (b.votes - a.votes))
+    });
+  }
   
   render() {
-
-    const list = posts
+    const list = this.state.posts
       .map((post, index) => 
         <li key={index}>
           <div className="container">
@@ -26,7 +42,7 @@ class App extends Component {
               <h4>{ post.title }</h4>
               <p>{ post.description }</p>
               <p className="graycolor">Escrito por {}
-                <img className="avatar" src={post.writer_avatar_url}/>
+                <img className="avatar" src={post.writer_avatar_url} alt={'avatar writer'}/>
               </p>
             </div>
           </div>  
@@ -36,6 +52,15 @@ class App extends Component {
       <div>
         <h1>Blog posts populares</h1>
         <ul>
+        <div className="container test">
+          <strong>Orden:</strong> 
+          <button onClick={this.sortByVotesAsc.bind(this)}>
+          Ascendente
+          </button>
+          <button onClick={this.sortByVotesDesc.bind(this)}>
+          Descendente
+          </button>
+        </div>
         {list}
         </ul>
       </div>   
